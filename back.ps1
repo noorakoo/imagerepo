02b1643a510415105478dcccef95bfc49f9b7d2a
noorakoo <nae.kataja@gmail.com>
2022-03-22 15:46:33 +0200

    created a backup and restore powershell script

--- back.ps1
+++ back.ps1
@@ -1,10 +1,10 @@
-$n = $args[0]
+$a = $args[0]
 
-foreach ($i in $n) {
-    if ($n -eq "backup") {
+foreach ($i in $a) {
+    if ($a -eq "backup") {
         docker exec b07479c3bb6e /usr/bin/mysqldump -u root --password=root imagerepo > backup.sql
         Write-Host "The database has been stored in backup.sql"
-    } elseif ($n -eq "restore") {
+    } elseif ($a -eq "restore") {
         Get-Content backup.sql | docker exec -i b07479c3bb6e /usr/bin/mysql -u root --password=root imagerepo
         Write-Host "The database has been restored from backup.sql"
     }
